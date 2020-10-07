331836651ef5197e923575d4a991e28038c6375c
Albert Vazquez <hola@albertvazquez.es>
2020-10-07 17:03:14 +0200

    bootstrap component

---
+++ src/components/CurrentCo2/CurrentCo2.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./CurrentCo2.css";
+export const CurrentCo2 = ({name}) => {
+    return (
+        <>
+            <h1 className="main-title">Hola {name}</h1>
+        </>
+    )
+}
