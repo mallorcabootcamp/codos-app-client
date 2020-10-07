8f73914879e88590d212b3a1846f58876aa99955
Ralvarez <R.alvgar@hotmail.com>
2020-10-07 18:20:35 +0200

    Pre-design of CurrentCo2

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import {CurrentCo2} from './components/CurrentCo2/CurrentCo2.jsx';
 function App() {
   return (
     <>
-      <h1>Hello!</h1>
-      <button className="btn btn-primary">Click me</button>
-      <CurrentCo2 name={'Roberto'}></CurrentCo2>
+      <CurrentCo2 eCoValue={'15'}></CurrentCo2>
     </>
   );
 }
--- src/components/CurrentCo2/CurrentCo2.jsx
+++ src/components/CurrentCo2/CurrentCo2.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import "./CurrentCo2.css";
-export const CurrentCo2 = ({name}) => {
+
+export const CurrentCo2 = ({eCoValue}) => {
     return (
-        <>
-            <h1 className="main-title">Hola {name}</h1>
-        </>
+        <div className='container'>
+            <div className='row m-5'>
+                <div className='col'>
+                    <h1 className="h3 text-left">Actual:</h1>
+                    <div className='row'>
+                        <div className='col text-center bg-success py-3 rounded'>
+                            <p className='h4 p-0 m-0'>eCo²</p>
+                            <p className='h3 p-0 m-0'>{eCoValue} ppm</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
