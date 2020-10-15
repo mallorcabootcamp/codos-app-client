d2230cb2717b4cc5bf63c924d39505b00e568336
Ralvarez <R.alvgar@hotmail.com>
2020-10-15 18:07:07 +0200

    Added React Router with provisional links

---
+++ src/screens/History/History.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const History = () => {
+    return (
+        <>
+
+            <Link to='/'>main </Link>
+
+            <h1>Historico</h1>
+        </>
+    );
+}
+
+export default History
