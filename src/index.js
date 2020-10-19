74ef095e000887e3ade0fdda6f2579e38510350d
Albert Vazquez <hola@albertvazquez.es>
2020-10-19 16:42:31 +0200

    use global font

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import './styles/index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-
-import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
