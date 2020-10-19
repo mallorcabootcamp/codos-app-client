9461829cc766d644266370a15dda6f0f62b0f643
Ralvarez <R.alvgar@hotmail.com>
2020-10-19 20:01:01 +0200

    folder name change

--- src/screens/History/History.jsx
+++ src/screens/History/History.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
-import { DatePicker } from '../../components/DateRangePickers/DatePicker';
+import { DatePicker } from '../../components/DatePickers/DatePicker';
 import './History.scss'
 
 const History = () => {
