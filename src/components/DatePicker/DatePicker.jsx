e754308c78c776712d6a1e57e42cf58ce33ec350
Ralvarez <R.alvgar@hotmail.com>
2020-10-21 19:36:13 +0200

    deleted unnecesary link

--- src/components/DatePicker/DatePicker.jsx
+++ src/components/DatePicker/DatePicker.jsx
@@ -6,9 +6,7 @@ export const DatePicker = ({ text, date, onDateChanged }) => {
   const idValue = useMemo(() => shortid.generate(), []);
   return (
     <>
-      <div>
         <label className='pt-2 date-picker-text' for={idValue}>{text}:</label>
-      </div>
         <input value={date} className='date-picker-input' onChange={({ target }) => { onDateChanged(target.value) }} name={text} type="date" id={idValue} />
     </>
   )
