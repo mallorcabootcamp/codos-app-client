32d775d0d1cb8e5fc145e3f9874d68277c738004
Ralvarez <R.alvgar@hotmail.com>
2020-10-21 17:57:33 +0200

    some css improvementes

--- src/components/CardWithTextTab/CardWithTextTab.jsx
+++ src/components/CardWithTextTab/CardWithTextTab.jsx
@@ -3,11 +3,11 @@ import './CardWithTextTab.scss';
 
 export const CardWithTextTab = ({ value ,children }) => {
     return (
-        <>
+        <div className='py-4'>
             <div className='card-tab inline d-inline-block px-3 py-1 text-center'>{value}</div>
             <div className='card-with-text-tab'>
                 {children}
             </div>
-        </>
+        </div>
     )
 }
