8c5a6efa12925a25193e6f836e1abb887a7bcec9
albertvazquezm <albertvazquezm@users.noreply.github.com>
2020-10-21 14:23:53 +0000

    deploy: b9cb18d7dbcf999c179258ad1f6266e2043a3e62

---
+++ precache-manifest.faca9ed71faaac3059011621ab290e43.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2efc292ee3df0c6ab4eb3709c38b2de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6929747e58eba824e317",
+    "url": "/static/css/main.d5aee28f.chunk.css"
+  },
+  {
+    "revision": "0d4423705da1c09d7089",
+    "url": "/static/js/2.4f08d37a.chunk.js"
+  },
+  {
+    "revision": "863e5dfaa7783b08dc1d03326e06085a",
+    "url": "/static/js/2.4f08d37a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6929747e58eba824e317",
+    "url": "/static/js/main.9e1778c6.chunk.js"
+  },
+  {
+    "revision": "927a259b524ffb047490",
+    "url": "/static/js/runtime-main.195ec056.js"
+  },
+  {
+    "revision": "d09098d2dad4db49a385be5715f64531",
+    "url": "/static/media/decoration.d09098d2.svg"
+  }
+]);
