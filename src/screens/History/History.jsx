f360aed6716198f3ea9bb4d457239b1f41885979
Ralvarez <R.alvgar@hotmail.com>
2020-10-21 17:20:27 +0200

    little changes to fix our online version

--- src/screens/History/History.jsx
+++ src/screens/History/History.jsx
@@ -22,32 +22,34 @@ const History = () => {
                 <h1 className='text-center history-text-elem'>Histórico</h1>
             </div>
             <div className='row date-range-pickers-container pt-3 mt-3'>
+                <div className="col mx-4 my-3">
                     <DatePicker date={fromDate} onDateChanged={(value) => setFromDate(value)} text='Desde' />
                     <DatePicker date={toDate} onDateChanged={(value) => setToDate(value)} text='Hasta' />
+                </div>
             </div>
             <div className='row'>
                 <div className="col pl-4 ml-3 my-3">
-                    <button className='btn search-button'>Buscar </button>
+                    <button className='search-button btn'>Buscar </button>
                 </div>
-            </div> 
+            </div>
             <CardWithTextTab value='eCO²'>
                 <div className="row">
                     <ParentSize className='history-graph-elem'>
-                        {({ width }) => <TimeWithValuesGraph endTimeValue={10} uom={'ppm'} timeFormat={'H:mm'} marginY={20} marginX={50} historicalValues={historicalValues} width={width-20} height={118} />}
+                        {({ width }) => <TimeWithValuesGraph endTimeValue={10} uom={'ppm'} timeFormat={'H:mm'} marginY={20} marginX={50} historicalValues={historicalValues} width={width - 20} height={118} />}
                     </ParentSize>
                 </div>
             </CardWithTextTab>
             <CardWithTextTab value='Temp.'>
                 <div className="row">
                     <ParentSize className='history-graph-elem'>
-                        {({ width }) => <TimeWithValuesGraph endTimeValue={10} uom={'ºC'} timeFormat={'H:mm'} marginY={20} marginX={50} historicalValues={historicalValues} width={width-20} height={118} />}
+                        {({ width }) => <TimeWithValuesGraph endTimeValue={10} uom={'ºC'} timeFormat={'H:mm'} marginY={20} marginX={50} historicalValues={historicalValues} width={width - 20} height={118} />}
                     </ParentSize>
                 </div>
             </CardWithTextTab>
             <CardWithTextTab value='xxx'>
                 <div className="row">
                     <ParentSize className='history-graph-elem'>
-                        {({ width }) => <TimeWithValuesGraph endTimeValue={10} uom={'xxx'} timeFormat={'H:mm'} marginY={20} marginX={50} historicalValues={historicalValues} width={width-20} height={118} />}
+                        {({ width }) => <TimeWithValuesGraph endTimeValue={10} uom={'xxx'} timeFormat={'H:mm'} marginY={20} marginX={50} historicalValues={historicalValues} width={width - 20} height={118} />}
                     </ParentSize>
                 </div>
             </CardWithTextTab>
