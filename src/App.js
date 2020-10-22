42da6bd70d399764377f7f638cd801d8286c07f4
Roberto Alvarez <r.alvgar@hotmail.com>
2020-10-22 20:36:25 +0200

    Added and working ScrollToTop

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { HashRouter, Switch, Route } from "react-router-dom";
 import Main from './screens/Main/Main';
 import History from './screens/History/History';
-import { ScrollToTop } from '../src/components/ScrollToTop';
+import { ScrollToTop } from '../src/components/ScrollToTop/ScrollToTop';
 import "./index.scss";
 
 function App() {
