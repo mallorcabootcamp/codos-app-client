cd125dc0d32d178bc129bf4c520f490a9a86ebd6
Ralvarez <R.alvgar@hotmail.com>
2020-11-04 18:46:10 +0100

    css and html added

--- src/screens/LateralBar/LateralBar.tsx
+++ src/screens/LateralBar/LateralBar.tsx
@@ -3,35 +3,35 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 import './LateralBar.scss';
 
-interface Props { onClick: any}
+interface Props { onClick: any }
 
 
-export const LateralBar = ({onClick}: Props):JSX.Element => {
+export const LateralBar = ({ onClick }: Props): JSX.Element => {
 
     return (
         <aside className="side-menu">
-            <header className='row px-5'>
-                {/* <i className="fas fa-bars"></i> */}
+            <header className="top d-flex p-3 pl-4 justify-content-between align-items-center">
                 <FontAwesomeIcon icon={faBars} size="lg" />
-                <p>NOMBRE DE LA APP</p>
 
-                <button className="close-button" onClick={onClick}>
-                    <FontAwesomeIcon icon={faTimesCircle} size='lg' />
-                </button>
+                <p className="m-0 font-weight-bold">NOMBRE DE LA APP</p>
+
+                <div className="close-button border-0" onClick={onClick}>
+                    <FontAwesomeIcon icon={faTimesCircle} size="lg" />
+                </div>
             </header>
 
-            <main className='row px-5'>
-                <p>Listado de dispositivos</p>
-                <button>Dispostivo 1</button>
-                <button>Dispostivo 2</button>
-                <button>Dispostivo 3</button>
+            <main className="menu mt-5 ml-4 d-flex flex-column ">
+                <p className="font-weight-bold ml-1">Listado de dispositivos</p>
+                <button className="menu-button border-0 my-2 font-weight-lighter">Dispositivo 1</button>
+                <button className="menu-button border-0 my-2 font-weight-lighter">Dispositivo 2</button>
+                <button className="menu-button border-0 my-2 font-weight-lighter mb-4">Dispositivo 3</button>
             </main>
 
-            <hr />
+            <hr className="ml-4 font-weight-bold" />
 
-            <footer className='row px-5'>
-                <a href="#">About</a>
+            <footer className="about mt-5 ml-4 font-weight-bold">
+                <p>About</p>
             </footer>
-        </aside>
+        </aside >
     )
 }
