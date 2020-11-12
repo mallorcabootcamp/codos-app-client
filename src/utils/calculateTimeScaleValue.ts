d5bdf5dc5a84ff50b9e14c4cd110b4010f584bee
Ralvarez <R.alvgar@hotmail.com>
2020-11-12 21:06:56 +0100

    better TimeScaleValue

--- src/utils/calculateTimeScaleValue.ts
+++ src/utils/calculateTimeScaleValue.ts
@@ -4,6 +4,11 @@ import moment from 'moment';
 export const calculateTimeScaleValue = (fromDate:string , toDate:string): string => {
     const timeRecivedDiference = Math.round(moment(toDate).valueOf()/1000) - Math.round(moment(fromDate).valueOf()/1000);
     const threeDaysDiference = moment().valueOf()/1000 - moment().subtract(3, 'days').valueOf()/1000;
-    const timeScaleValue = timeRecivedDiference < threeDaysDiference ? "60m" : "1d";
-    return timeScaleValue;
+    const threeMonthsDiference = moment().valueOf()/1000 - moment().subtract(3, 'months').valueOf()/1000;
+    const sevenMonthsDiference = moment().valueOf()/1000 - moment().subtract(7, 'months').valueOf()/1000;
+    if (timeRecivedDiference < threeDaysDiference) return '1h';
+    else if(timeRecivedDiference > threeDaysDiference && timeRecivedDiference < threeMonthsDiference) return '1d';
+    else if(timeRecivedDiference > threeMonthsDiference && timeRecivedDiference < sevenMonthsDiference) return '1w'
+    else return'4w'
+
 }
